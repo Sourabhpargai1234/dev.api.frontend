8027ae52b12e922ec514f78b332aa356e8d81735
Sourabh <Sourabhpar@gmail.com>
2024-06-30 19:03:15 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
         </button>
       </div>
       {isLoading ? (
-        <div className="flex justify-center items-center h-64">
+        <div className="flex justify-center items-center flex-col h-64">
           <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-24 w-24"></div>
           <h2>Loading...</h2>
         </div>
